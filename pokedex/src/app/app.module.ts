1f95ef535301374306fc8fb52f41e66b75e80aca
Fernando Schimidt <fernandoschimidt@outlook.com>
2021-09-29 19:14:36 -0300

    input

---
+++ pokedex/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+//Modules
+import { PagesModule } from './pages/pages.module';
+
+//Componentes
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ pokedex/src/app/pages/pages.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//modules routing
+import { RoutingModule } from './routing.modules';
+
+
+//Pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
